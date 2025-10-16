46a9dbb4e5b28d183099b982d3abd064a703225d
Nikhil Mahato <nikhilmahato104@gmail.com>
2025-10-17 00:50:35 +0530

    checkeing 2

--- server.js
+++ server.js
@@ -64,9 +64,11 @@ const allowedOrigins = [
   'http://localhost:3001',
   'http://192.168.137.1:5173',
   'https://b-to-b-apixvuzum.onrender.com',
+  'https://b-to-b-apixvuzum-ag91.onrender.com',
   'https://fzt-api-frilu457.onrender.com',
   'https://sales-manpanel-react-darkhower.netlify.app',
   'https://admin.flipzonto.com',
+
 ];
 
 
